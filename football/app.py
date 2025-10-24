083aa54393fd2409b3c64cc2699131cf8608c953
Jordan B <54034594+Geedorah54@users.noreply.github.com>
2025-10-24 10:27:45 -0500

    Update app.py with prediction update information
    Added a note about prediction updates before TNF.

--- football/app.py
+++ football/app.py
@@ -9,6 +9,10 @@ st.write("""
 This is a weekly summary of my NFL moneyline predictions.  No machine learning model is %100 - bet at your own risk!
 """)
 
+st.write("""
+Predictions will be updated on Thursdays before TNF, and capture every game for the weekend including Monday Night.
+""")
+
 # Load the data
 df = pd.read_csv("football/weekly_prediction.csv")
 
