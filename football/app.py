4c059aeccc75d4acf89acea7587ca0b400e0c15f
Jordan B <54034594+Geedorah54@users.noreply.github.com>
2025-10-24 10:28:09 -0500

    Update app.py

--- football/app.py
+++ football/app.py
@@ -6,11 +6,11 @@ st.set_page_config(page_title="Football Moneyline Predictions", layout="wide")
 st.title("🏈 Jordan's Weekly Picks")
 
 st.write("""
-This is a weekly summary of my NFL moneyline predictions.  No machine learning model is %100 - bet at your own risk!
+- This is a weekly summary of my NFL moneyline predictions.  No machine learning model is %100 - bet at your own risk!
 """)
 
 st.write("""
-Predictions will be updated on Thursdays before TNF, and capture every game for the weekend including Monday Night.
+- Predictions will be updated on Thursdays before TNF, and capture every game for the weekend including Monday Night.
 """)
 
 # Load the data
